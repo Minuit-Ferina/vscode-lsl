19b59e972155c361ab22dfc489a4a61c58b58205
Minuit <minuit.ferina@gmail.com>
2023-02-14 01:41:47 +0100

    add the missing Functions and Constants imports

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,8 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { Functions } from './Functions'
+import { Constants } from './Constants'
 
 import {
 	LanguageClient,
@@ -165,6 +166,124 @@ vscode.languages.registerCompletionItemProvider("lsl", {
 		return list;
 	}
 })
+
+vscode.languages.registerSignatureHelpProvider('lsl', {
+	provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+
+		let resource = document.uri;
+
+		const line = document.lineAt(position).text;
+		const lineTrimmed = line.substring(0, position.character);
+		const len = lineTrimmed.length;
+
+		// borrowed from https://github.com/maziac/binary-file-viewer/blob/main/src/completionprovider.ts
+		// Remove hyphens (everything in between ' and ")
+		let modLine = '';
+		let k = 0;
+		while (k < len) {
+			const char = lineTrimmed.substring(k, k + 1);
+			if (char == "'" || char == '"') {
+				do {
+					k++;
+					if (k >= len)
+						break;
+					const nChar = lineTrimmed.substring(k, k + 1);
+					if (nChar == char)
+						break;
+				} while (k < len);
+				k++;
+				continue;
+			}
+			modLine += char;
+			k++;
+		}
+
+
+		// Search reverse for first unclosed bracket (
+		let i = modLine.length - 1;
+		let bracketCount = 0;
+		while (i >= 0) {
+			const char = modLine.substring(i, i + 1);
+			if (char == '(') {
+				if (bracketCount == 0)
+					break;
+				bracketCount--;
+			}
+			else if (char == ')') {
+				bracketCount++;
+			}
+			i--;
+		}
+
+		// Get the word before the bracket
+		const beforeLine = modLine.substring(0, i).trimEnd();	// without bracket
+		//console.log('SignatureProvider : provideSignatureHelp : beforeLine', beforeLine);
+
+		// Get the function name
+		const matchLast = /[\w.]+$/g.exec(beforeLine);
+		if (!matchLast)
+			return undefined;
+		const funcName = matchLast[0];
+		//console.log('SignatureProvider : provideSignatureHelp : funcName', funcName);
+
+		// Get the number of parameters
+		const afterLine = modLine.substring(i + 1);
+		const countColons = afterLine.split(',').length - 1;
+
+
+		let e = Functions.filter(function (el) {
+			return el.name === funcName;
+
+		})
+
+		let ret: vscode.SignatureHelp = {
+			activeSignature: 0,
+			activeParameter: countColons,
+			signatures: []
+		};
+
+		if (e.length > 0) {
+			let a = e[0]
+			let signature = getFunctionSignature(a)
+
+			let param = []
+
+			for (let e of a.param) {
+				param.push(
+					{
+						label: `${e["type"]} ${e["name"]}`,
+						documentation: e["description"]
+					}
+				)
+			}
+
+
+			ret.signatures = [{
+				label: signature,
+				parameters: param
+			}]
+		}
+
+		return ret;
+
+	}
+},
+	'(', ',')
+
+
+function getFunctionSignature(fn: object): string {
+	let ret = `${fn["returnType"]}`
+	if (fn["returnType"] != "")
+		ret += " "
+	ret += `${fn["name"]}(`
+	for (let c = 0; c < fn["param"].length; c++) {
+		ret += `${fn["param"][c]["type"]} ${fn["param"][c]["name"]}`
+		if (c < fn["param"].length - 1)
+			ret += ", "
+	}
+	ret += `)`
+	return ret
+}
 
 export function deactivate(): Thenable<void> | undefined {
 	if (!client) {
