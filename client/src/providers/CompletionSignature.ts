940b7eb18a529dc89466cf4598ecc3afbb856ecd
Minuit <minuit.ferina@gmail.com>
2023-06-30 21:36:46 +0200

    update to the new parser

--- client/src/providers/CompletionSignature.ts
+++ client/src/providers/CompletionSignature.ts
@@ -1,5 +1,5 @@
 import { Token, CommonToken, ParseTreeVisitor, CharStream, CommonTokenStream, ParseTreeWalker, FileStream, ParseTree, TerminalNode, ErrorNode, ErrorListener, RecognitionException, Recognizer } from 'antlr4';
-import LSLParser, { Compound_statementContext, DeclarationContext, Default_stateContext, EventContext, Function_parameterContext, Function_parametersContext, GlobalContext, Global_functionContext, Global_variableContext, IdentifierContext, LlstateContext, Lscript_programContext, Name_typeContext } from '../antlr4/LSLParser';
+import LSLParser, {  } from '../antlr4/LSLParser';
 import LSLVisitor, { } from '../antlr4/LSLParserVisitor';
 
 
