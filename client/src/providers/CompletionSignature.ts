6124e23eb68b7ea6eaf81a1110e9ba93c744a7ff
Minuit <minuit.ferina@gmail.com>
2023-07-01 01:50:33 +0200

    remove unused file

--- client/src/providers/CompletionSignature.ts
+++
@@ -1,5 +0,0 @@
-import { Token, CommonToken, ParseTreeVisitor, CharStream, CommonTokenStream, ParseTreeWalker, FileStream, ParseTree, TerminalNode, ErrorNode, ErrorListener, RecognitionException, Recognizer } from 'antlr4';
-import LSLParser, {  } from '../antlr4/LSLParser';
-import LSLVisitor, { } from '../antlr4/LSLParserVisitor';
-
-
